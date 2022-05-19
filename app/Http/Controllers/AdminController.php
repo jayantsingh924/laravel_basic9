e0c5a33ee4c452c85c56e863eb0eac0faf7cb576
Jayant924 <jayantsingh924@gmail.com>
2022-05-20 02:55:14 +0530

    Master Layout dor Dashboard

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
 
 class AdminController extends Controller
 {
-    //
+
+    public function adminDashboard()
+    {
+        return view('admin.index');
+    }
+
+    public function destroy(Request $request)
+    {
+        Auth::guard('web')->logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
 }
